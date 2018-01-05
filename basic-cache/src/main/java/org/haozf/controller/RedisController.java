80d563b0dc6c4a00b3af17f5e1bc5db9924d9c28
haozhengfeng <haozhengfeng123@163.com>
2018-01-05 22:53:43 +0800

    增加redis缓存模块

--- basic-cache/src/main/java/org/haozf/controller/RedisController.java
+++ basic-cache/src/main/java/org/haozf/controller/RedisController.java
@@ -4,9 +4,9 @@ import org.haozf.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 
  * redis restfull 对外服务接口
  */
 @Controller
@@ -16,8 +16,30 @@ public class RedisController {
     RedisService redisService;
     
     @RequestMapping("exists")
+    @ResponseBody
     public String exists(String key){
         return Boolean.toString(redisService.exists(key));
+    }
+    
+    @RequestMapping("get")
+    @ResponseBody
+    public String get(String key){
+        if(redisService.exists(key))
+            return redisService.get(key).toString();
+        return "";
+    }
+    
+    @RequestMapping("set")
+    @ResponseBody
+    public String set(String key,String value,long expire){
+        return Boolean.toString(redisService.set(key, value, expire));
+    }
+    
+    @RequestMapping("remove")
+    @ResponseBody
+    public String remove(String key){
+        redisService.remove(key);
+        return "true";
     }
     
 }
