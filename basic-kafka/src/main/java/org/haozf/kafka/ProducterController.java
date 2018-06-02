872c937d39d35d17deb8c0a306c3f1a26462a7e5
haozhengfeng <haozhengfeng123@163.com>
2018-06-02 10:10:57 +0800

    更新

--- basic-kafka/src/main/java/org/haozf/kafka/ProducterController.java
+++ basic-kafka/src/main/java/org/haozf/kafka/ProducterController.java
@@ -3,21 +3,24 @@ package org.haozf.kafka;
 import java.util.Map;
 
 import org.haozf.kafka.producer.KafkaProducerServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProducterController {
-	
+
+	protected final Logger LOG = LoggerFactory.getLogger("ProducterController");
+
 	@Autowired
 	KafkaProducerServer kafkaProducer;
 
 	@RequestMapping("/sendMsg")
-    @ResponseBody
-    public void sendMsg(String msg){
+	@ResponseBody
+	public void sendMsg(String msg) {
 		String topic = "test";
 		String value = "test";
 		String ifPartition = "1";
@@ -25,12 +28,12 @@ public class ProducterController {
 		String role = "test";// 用来生成key
 		Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value, ifPartition, partitionNum, role);
 
-		System.out.println("测试结果如下：===============");
+		LOG.info("测试结果如下：===============");
 		String message = (String) res.get("message");
 		String code = (String) res.get("code");
 
-		System.out.println("code:" + code);
-		System.out.println("message:" + message);
-    }
-	
+		LOG.info("code:" + code);
+		LOG.info("message:" + message);
+	}
+
 }
